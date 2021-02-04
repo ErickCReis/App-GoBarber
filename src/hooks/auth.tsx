02e9f933c1d0694ca81df654652bf48c2959cd83
ErickCReis <erickcorreareis@gmail.com>
2021-02-03 21:34:54 -0300

    :lipstick:  adding dashboard header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,15 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SigInCredentials {
@@ -19,7 +25,7 @@ interface SigInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SigInCredentials): Promise<void>;
   signOut(): void;
