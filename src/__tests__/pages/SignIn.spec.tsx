3ab627ee1c087bff0989c28bea98da712706aba4
ErickCReis <erickcorreareis@gmail.com>
2021-02-07 15:06:50 -0300

    :white_check_mark:  adding simple sign in page test

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: () => {
+      return { navigate: jest.fn() };
+    },
+  };
+});
+
+describe('SignIn page', () => {
+  it('should contains email/password inputs', () => {
+    const { getByPlaceholderText } = render(<SignIn />);
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+    expect(getByPlaceholderText('Senha')).toBeTruthy();
+  });
+});
