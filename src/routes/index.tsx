649abd12a56ba3e27fff6add03cf51a24bcd2f8b
ErickCReis <erickcorreareis@gmail.com>
2021-02-12 22:08:54 -0300

    :sparkles:  adding react native splash screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
@@ -9,13 +9,9 @@ import { useAuth } from '../hooks/auth';
 const Routes: React.FC = () => {
   const { user, loading } = useAuth();
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ActivityIndicator size="large" color="#999" />
-      </View>
-    );
-  }
+  useEffect(() => {
+    if (!loading) SplashScreen.hide();
+  }, [loading]);
 
   return user ? <AppRoutes /> : <AuthRoutes />;
 };
